c10c5bef57b46f84d818fdba3c64596f191c9bc2
CaioARosa <caioazul.bh@gmail.com>
2025-08-22 14:35:29 -0300

    Plans Page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RegisterService } from './register.service';
 import { LoadingService } from '../../core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
   standalone: true,
@@ -12,7 +15,8 @@ import { LoadingService } from '../../core';
 })
 export class Register implements OnInit {
 
-  public planos: any = [];
+  public planos: any[] = [];
+  public freeplan: boolean = false;
 
   constructor(
     private registerService: RegisterService,
@@ -27,7 +31,8 @@ export class Register implements OnInit {
     this.loadingService.show();
     this.registerService.getPlanos().subscribe({
       next: (response: any) => {
-        this.planos = response;
+        this.planos = response.data;
+        this.freeplan = this.planos.some((p: any) => p.valor_mensal == 0)
         this.loadingService.hide();
       },
       error: (error) => {
